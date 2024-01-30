c0a90a9496593e6d4145cd45923accd3c8c92dc7
maripiek <mariusz.w.piekarski@gmail.com>
2024-01-30 20:48:11 +0100

    Fixed eslint warning

--- src/planets/DryPlanet.tsx
+++ src/planets/DryPlanet.tsx
@@ -1,17 +1,16 @@
 import { useFrame } from "@react-three/fiber";
 import React from "react";
-import { Vector4 } from "three";
 import DryPlanetLayer from "../layers/DryPlanetLayer";
 
 export default function DryPlanet() {
 
   const myGroup = React.useRef<any>()
 
-  const colors = [
-    new Vector4(102 / 255, 176 / 255, 199 / 255, 1),
-    new Vector4(102 / 255, 176 / 255, 199 / 255, 1),
-    new Vector4(52 / 255, 65 / 255, 157 / 255, 1)
-]
+//   const colors = [
+//     new Vector4(102 / 255, 176 / 255, 199 / 255, 1),
+//     new Vector4(102 / 255, 176 / 255, 199 / 255, 1),
+//     new Vector4(52 / 255, 65 / 255, 157 / 255, 1)
+// ]
   
   useFrame(({ clock }) => {
     myGroup.current.children.forEach((layer: { material: { uniforms: { [x: string]: { value: number; }; }; }; }) => {
